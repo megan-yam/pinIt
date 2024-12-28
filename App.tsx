18708329f5d7b37a7e74d4c6fcbeeafda3075182
Megan Yam <119703973+megan-yam@users.noreply.github.com>
2024-12-27 23:21:50 -0800

    added files

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import Mapbox, { UserLocation } from "@rnmapbox/maps";
 
-export default function App() {
+Mapbox.setAccessToken(
+  "pk.eyJ1IjoibWVnYW55YW0iLCJhIjoiY201MGh1OW1wMWdsNjJub2RnemFnODZieiJ9.WrX1uSxj4JO8BG80gI-vkA"
+);
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Mapbox.MapView style={styles.map} styleURL={Mapbox.StyleURL.Street}>
+        <UserLocation />
+      </Mapbox.MapView>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    flex: 1,
   },
 });
